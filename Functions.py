d0a9f0abe73e793bbdd12dce9a811e46936dcf42
Oliver Quinonez <oliver.quinonez@colorado.edu>
2024-10-16 23:29:17 -0600

    Radial Problem Solved

--- Functions.py
+++ Functions.py
@@ -7,7 +7,10 @@ import xarray as xr
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import scipy.integrate as scint
+import scipy.interpolate as scinterp
 xr.set_options(keep_attrs=True)
+import cmath
+import scipy
 
 #########################################################################################################################
 
@@ -30,7 +33,11 @@ omega_355 = lambda_to_freq(lambda_355)  # Hz
 omega_118 = lambda_to_freq(lambda_355)  # Hz
 mXe = 2.1801714E-25  # kg
 
+
+k118 = 2*np.pi/(118*10**(-9))
+
 nonzero = 1e-10
+
 
 #########################################################################################################################
 
@@ -66,8 +73,8 @@ def ReJ3(z,z0,dk,params): # Returns the integral of the real part of J_3 from bo
     b= params['b']
     Ref1 = lambda x: (1/(1+ (2*x/b)**2)**2) * (1-(2*x/b)**2) #times cos(dk*x)
     Ref2 = lambda x: (1/(1+ (2*x/b)**2)**2) * (2*(2*x/b)) #times sin(dk*x)                                 
-    I1 = scint.quad(Ref1,z0,z,weight ='cos',wvar = dk,epsabs = 1e-6,epsrel=1e-6)
-    I2 = scint.quad(Ref2,z0,z, weight = 'sin', wvar =dk,epsabs = 1e-6,epsrel=1e-6)
+    I1 = scint.quad(Ref1,z0,z,weight ='cos',wvar = dk,epsabs = 1e-6,epsrel=1e-8,limit =8000)
+    I2 = scint.quad(Ref2,z0,z, weight = 'sin', wvar =dk,epsabs = 1e-6,epsrel=1e-8, limit = 8000)
     Itot = I1[0]+I2[0]
     return Itot
 
@@ -76,13 +83,14 @@ def ImJ3(z,z0,dk,params):# Returns the integral of the real part of J_3 from boy
     b= params['b']
     Imf1 = lambda x: (1/(1+(2*x/b)**2)**2) * (1-(2*x/b)**2) #times sin(dk*x)
     Imf2 = lambda x: (1/(1+(2*x/b)**2)**2)* (-(2*(2*x/b))) #times *np.cos(dk*x)) 
-    I1 = scint.quad(Imf1,z0,z, weight = 'sin', wvar = dk,epsabs = 1e-6,epsrel=1e-6)
-    I2 = scint.quad(Imf2,z0,z, weight = 'cos', wvar = dk,epsabs = 1e-6,epsrel=1e-6)
+    I1 = scint.quad(Imf1,z0,z, weight = 'sin', wvar = dk,epsabs = 1e-6,epsrel=1e-8)
+    I2 = scint.quad(Imf2,z0,z, weight = 'cos', wvar = dk,epsabs = 1e-6,epsrel=1e-8)
     Itot= I1[0]-I2[0]
     return Itot
 
 def phi3(z,z0,dk,params): #return the phase of the complex conjugate of J3
-    return np.arctan2(-ImJ3(z,z0,dk,params),ReJ3(z,z0,dk,params))
+    J = complex(ReJ3,ImJ3)
+    return -cmath.phase(J)
 
 def evalRef(x,dk,params):
     b= params['b']
@@ -117,19 +125,66 @@ def phi3_gauss_quad(z,z0,dk,params): #return the phase of the complex conjugate 
 def phi3_gauss_quad_arctan1(z,z0,dk,params):
     return np.arctan(-ImJ3_gauss_quad(z,z0,dk,params)/ReJ3_gauss_quad(z,z0,dk,params))
 
+def phi3_gauss_quad_phase(z,z0,dk,params):
+    w = np.complex(ReJ3_gauss_quad(z,z0,dk,params),-ImJ3_gauss_quad(z,z0,dk,params))
+    return np.phase(w)
+
+
+
+def phi3_gauss_quad_complex(z,z0,dk,params):
+    b = params['b']
+    complex_f = lambda x: (np.cos(x*dk) + 1j*np.sin(x*dk))/(1+1j*(2*x/b))**2
+
+    def Ref(x):
+        return scipy.real(complex_f(x))
+    def Imf(x):
+        return scipy.imag(complex_f(x))
+
+    ReJ = scint.quadrature(Ref,z0,z,rtol = 1e-5,tol =1e-8, maxiter =300)
+    ImJ = scint.quadrature(Imf,z0,z,rtol = 1e-5,tol =1e-8, maxiter =300)
+
+    J = complex(ReJ[0],ImJ[0])
+    return -cmath.phase(J)
+
+
+
+def phi3_interp(zval,Phi,params):
+    z = params['z']
+    phi3_interp = scinterp.interp1d(z, Phi, kind='cubic', fill_value='extrapolate')
+    return phi3_interp(zval)
+
+
 #########################################################################################################################
 
-def dA118_dz_GBNA(r,z0,z,amplitudes,params):
+def dA118_dz_GBNA(r,Phi,z0,z,amplitudes,params):
     chi3 = params['chi3']
     PXe = params['PXe'] * Torr_to_m3
     k118 = 2*np.pi/(118*10**(-9))
     b = params['b']
     
     [A_118, _] = amplitudes
-    
+
+
+
     dA_118_dz = (1/2)*chi3*PXe*k118 * (amplitude_355(r,z,params)**3) \
-    * np.cos((2*z/b)-2*np.arctan2(2*z/b,1)+phi3(z,z0,2/b,params))\
-     +(dBeam_radius_dz(z, params)/beam_radius(z,params))*(((6*r**2)/(beam_radius(z,params))**2)-2)*A_118
+    * np.cos((2*z/b)-2*np.arctan2(2*z/b,1) + phi3_interp(z,Phi,params))\
+     +(dBeam_radius_dz(z, params)/beam_radius(z,params))*(((6*r**2)/(beam_radius(z,params))**2)-1)*A_118
+   
+    dA_fluo_dz = 0
+    
+    return [dA_118_dz, dA_fluo_dz]
+
+def dA118_dz_GBNA_curly(Phi,z0,z,amplitudes,params):
+    chi3 = params['chi3']
+    PXe = params['PXe'] * Torr_to_m3
+    k118 = 2*np.pi/(118*10**(-9))
+    b = params['b']
+    omega0 = params['omega0']
+    
+    [A_118, _] = amplitudes
+
+    dA_118_dz = (1/2)*chi3*PXe*k118 * (omega0/beam_radius(z,params))**2 * (peak_amplitude_355(params)**3) \
+    * np.cos((2*z/b)-2*np.arctan2(2*z/b,1) + phi3_interp(z,Phi,params))\
    
     dA_fluo_dz = 0
     
@@ -141,12 +196,13 @@ def dA118_dz_GBWA(r,z0,z,amplitudes,params): #extra term to make it 3d
     PXe = params['PXe'] * Torr_to_m3
     omega0 = params['omega0']
     b = params['b']
+    zsamples = params['zsamples']
     k118 = 2*np.pi/(118*10**(-9))
     
     [A_118, _] = amplitudes
-    
+
     dA_118_dz = (1/2)*chi3*PXe*k118 * (amplitude_355(r,z,params)**3) \
-    * np.cos((2*z/b)-2*np.arctan2(2*z/b,1)+phi3(z,z0,2/b,params))\
+    * np.cos((2*z/b)-2*np.arctan2(2*z/b,1)+phi3_gauss_quad_complex(z,z0,2/b,params))\
      -(dBeam_radius_dz(z, params)/beam_radius(z,params))*(1-((6*r**2)/(beam_radius(z,params))**2))*A_118 \
     -  0.5*sigma * PXe**2 * A_118
     
@@ -154,16 +210,26 @@ def dA118_dz_GBWA(r,z0,z,amplitudes,params): #extra term to make it 3d
     
     return [dA_118_dz, dA_fluo_dz]
 
-def solve_diff_eq(func, params, zrange, init_vals, z_eval,r_eval):
-    
+def solve_diff_eq_3D(func, params, zrange, init_vals, z_eval,r_eval):
+
+    b = params['b']
+    z = params['z']
+    zsamples = params['zsamples']
+    zstart = params['zstart']
+
+    Phi = np.zeros(len(z))
+    i =0
+    for zval in z:
+        Phi[i] = phi3_gauss_quad_complex(zval, zstart, 2/b, params)
+        i += 1
+
     beam_118_array = np.zeros((len(r_eval),len(z_eval)),np.longdouble)
     fluor_array = np.zeros((len(r_eval),len(z_eval)))
     arr_index = 0
     (zstart, zstop) = zrange
-    
     for r in r_eval:
-        sol = scint.solve_ivp(functools.partial(func, r, zstart+nonzero, params=params), 
-                              zrange, init_vals, t_eval=z_eval,method ='Radau',rtol = 1e-5,atol = 1e-16) # seems like r problem was stiff
+        sol = scint.solve_ivp(functools.partial(func, r, Phi,zstart+nonzero,params=params), 
+                            zrange, init_vals, t_eval=z_eval,method ='Radau',rtol = 1e-5,atol = 1e-10) # seems like r problem was stiff
         beam_118_array[arr_index] = sol['y'][0]
         fluor_array[arr_index] = sol['y'][1]
         arr_index += 1
@@ -171,6 +237,54 @@ def solve_diff_eq(func, params, zrange, init_vals, z_eval,r_eval):
     beam_118 = xr.DataArray(beam_118_array, 
                             dims = ('r','z'), 
                             coords = {'z': sol['t']},
+                            attrs = {'units': 'V/m',
+                                     'long_name': "118 nm amplitude"})
+    fluor = xr.DataArray(fluor_array, 
+                            dims = ('r','z'), 
+                            coords = {'z': sol['t']},
+                            attrs = {'units': 'arb.',
+                                     'long_name': "Fluorescence"})
+    data = xr.Dataset(
+        data_vars = {#'beam_355': beam_355,
+                     'beam_118': beam_118,
+                     'fluor': fluor},
+        attrs = {'z_range': zrange, 'init_vals': init_vals, **params}
+        )
+    return data
+
+def solve_diff_eq_1D(func, params, zrange, init_vals, z_eval,r_eval):
+
+    b = params['b']
+    z = params['z']
+    r = params['r']
+    zsamples = params['zsamples']
+    zstart = params['zstart']
+    omega0 = params['omega0']
+
+    Phi = np.zeros(len(z))
+    i =0
+    for zval in z:
+        Phi[i] = phi3_gauss_quad_complex(zval, zstart, 2/b, params)
+        i += 1
+
+    arr_index = 0
+    (zstart, zstop) = zrange
+    sol = scint.solve_ivp(functools.partial(func, Phi,zstart+nonzero,params=params), 
+                        zrange, init_vals, t_eval=z_eval,method ='Radau')
+    
+    beam_118_array = np.zeros((len(r_eval),len(z_eval)),np.longdouble)
+    fluor_array = np.zeros((len(r),len(z)))
+    arr_index = 0
+    for rval in r:
+        beam_118_array[arr_index,:] = sol['y'][0]*(omega0/beam_radius(z,params))*np.exp(-3*rval**2/beam_radius(z,params)**2)
+        #fluor_array[arr_index] = sol['y'][1]
+        arr_index += 1
+    
+    
+           
+    beam_118 = xr.DataArray(beam_118_array, 
+                            #dims = ('r','z'), 
+                            #coords = {'z': sol['t']},
                             attrs = {'units': 'V/m',
                                      'long_name': "118 nm amplitude"})
     fluor = xr.DataArray(fluor_array, 
